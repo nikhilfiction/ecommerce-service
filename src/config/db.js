cb1ebb4c380f0288c22edb2d09bb1ec42edde205
nikhilfiction <nikhilsahu3993@gmail.com>
2025-09-18 02:45:51 +0530

    Added db connection

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose= require("mongoose")
+const dotenv= require("dotenv")
+dotenv.config()
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Mongodb connected")
+    }catch(error) {
+        console.error('Mongodb connection failed due to:', error);
+        process.exit(1);
+
+
+
+    }
+}
+module.exports= connectDB
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-const express = require("express")
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-const app= express()
-
+const app= express();
+app.use(morgan("dev"));
+connectDB()
 app.get("/", (req, res) => {
     res.send("E-commerce service backend is running....")
 })
