3c94d32188644a9e6f7cdd29c5ff14087a108ccb
nikhilfiction <nikhilsahu3993@gmail.com>
2025-09-26 14:20:02 +0530

    Added cors

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const productRoutes = require('./routes/productRoutes');
+const cors= require('cors')
 require('dotenv').config();
 const app= express();
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors())
 connectDB()
 
 const PORT= process.env.PORT || 3000
