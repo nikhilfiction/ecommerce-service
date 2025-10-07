6021f3a32c4350c0fd810c71e662fb9f861b29bb
nikhilfiction <nikhilsahu3993@gmail.com>
2025-10-07 23:51:53 +0530

    Added checkout and payment gateway

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -23,26 +23,5 @@ const authMiddleware= (req, res, next) => {
 
 module.exports = authMiddleware
 
-// const jwt = require('jsonwebtoken')
-// require('dotenv').config()
-
-// const authMiddleware= (req, res, next) => {
-//     const authHeader = req.headers.authorization;
-
-//     if(!authHeader) {
-//         return res.status(401).json({message: "No token provided"})//"Bearer<JWT token"
-//     }
-
-//     const token = authHeader.split(" ")[1];
-
-//     try {
-//         const decoded= jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = decoded
-//         next();
-//     } catch(error) {
-//         return res.status(401).json({message: "Invalid token"})
-//     }
-// }
 
 
-// module.exports = authMiddleware
